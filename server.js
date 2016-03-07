c4cdbbfb453edaf91d19f770d2ac6c4c05de5abc
Kaley <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-03-07 15:02:07 -0500

    first files

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static("public"));
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(process.env.PORT || 5000, function(){
+  console.log('listening on *:5000');
+});
