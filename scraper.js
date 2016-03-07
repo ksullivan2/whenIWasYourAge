400a5bb89e66c249b7794b85fbf581e72d1ec74e
Kaley <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-03-07 15:50:56 -0500

    wrapped cheerio in a function

--- scraper.js
+++ scraper.js
@@ -3,14 +3,11 @@ var rp = require('request-promise');
 
 function scrapePage(url){
 	//rp says "once this is done, do the .then function"
-	return rp(url)
+	return createCheerio(url)
 
 	//once that loads in...
-	.then(function(content){
+	.then(function($){
 
-		//$ refers to the full loaded content of the page
-		$ = cheerio.load(content, {
-			normalizeWhitespace: true});
 		 
 		var yearEvents = [] 
 
@@ -21,14 +18,33 @@ function scrapePage(url){
 			});
 
 		})
+		//console.log(yearEvents);
+		return yearEvents;
+	})
+	
+}
+
+function createCheerio(url){
+	//rp says "once this is done, do the .then function"
+	return rp(url)
+
+	//once that loads in...
+	.then(function(content){
+
+		//$ refers to the full loaded content of the page
+		$ = cheerio.load(content, {
+			normalizeWhitespace: true});
+
+		return $
+	})
+}
 
 
-		console.log(yearEvents)
-		
+function whatLinksHere(href){
+	return createCheerio(href)
+	.then(function($){
 
 	})
-
-
 }
 
 
