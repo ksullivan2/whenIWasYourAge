cf013a3012535c1ccceb88d78e38470e24333ecb
Kaley <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-03-13 18:22:22 -0400

    slider doens't look horrible

--- reactApp/components/Slider.js
+++ reactApp/components/Slider.js
@@ -5,11 +5,20 @@ var EndYear = require('./EndYear');
 
 var Slider = React.createClass({
 
+
   render: function () {
+  	var sliderLength = (this.props.endYear-this.props.birthYear);
+
     return (
       <div id='Slider'>
-        Slider
         <BirthYear birthYear={this.props.birthYear} />
+        <div id="yearSliderDIV">
+	        <input id="yearSlider"
+	        	type="range" 
+	        	defaultValue="16" 
+	        	min= "0"
+	        	max= {sliderLength}/>
+	       </div>
         <EndYear endYear={this.props.endYear} />
       </div>
     )
