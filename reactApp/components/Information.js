54b20908b83c54eba1eb8e64416dbb668b6032e5
Kaley <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-03-13 16:54:54 -0400

    passes props down from "information"

--- reactApp/components/Information.js
+++ reactApp/components/Information.js
@@ -5,13 +5,22 @@ var Change = require('./Change');
 var Name = require('./Name');
 
 var Information = React.createClass({
+  getInitialState: function(){
+    return{
+      name:"Tom",
+      birthYear: "1956",
+      endYear: "2016"
+    }
+  },
+
+
   render: function () {
     return (
       <div id='Information'>
         Information
-        <Slider />
+        <Slider birthYear={this.state.birthYear} endYear={this.state.endYear}/>
         <Change />
-        <Name />
+        <Name name={this.state.name}/>
       </div>
     )
   }
--- reactApp/components/Slider.js
+++ reactApp/components/Slider.js
@@ -4,12 +4,20 @@ var BirthYear = require('./BirthYear');
 var EndYear = require('./EndYear');
 
 var Slider = React.createClass({
+
+	getInitialState: function(){
+		return{
+			birthYear: this.props.birthYear,
+			endYear: this.props.endYear
+		}
+	},
+
   render: function () {
     return (
       <div id='Slider'>
         Slider
-        <BirthYear />
-        <EndYear />
+        <BirthYear birthYear={this.state.birthYear} />
+        <EndYear endYear={this.state.endYear} />
       </div>
     )
   }
